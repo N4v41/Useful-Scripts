101e6bf60a4133c3c37e43d80fc2f32db10321a1
N4v41 <45365461+N4v41@users.noreply.github.com>
2020-01-22 12:24:19 -0300

    Update webmin version

--- linux/debian based/install_webmin.sh
+++ linux/debian based/install_webmin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mkdir /home/bin/webmin
 cd /home/bin/webmin
-sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.930_all.deb
+sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.941_all.deb
 sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip
-sudo dpkg --install webmin_1.930_all.deb
+sudo dpkg --install webmin_1.941_all.deb
